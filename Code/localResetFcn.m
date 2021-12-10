d1aca50f16f973322c69a55f711ceb4a5766b3cb
Lechili <95209547+Lechili@users.noreply.github.com>
2021-12-10 10:38:20 +0100

    Update localResetFcn.m
    updated local reset function

--- Code/localResetFcn.m
+++ Code/localResetFcn.m
@@ -1,4 +1,6 @@
 function in = localResetFcn(in)
 % Reset the initial position of the lead car.
 in = setVariable(in,'x0_lead',40+randi(60,1,1));
+in = setVariable(in,'v0_lead',randi(20,1,1)-1);
+%in = setVariable(in,'v0_ego',randi(30,1,1)-1);
 end
